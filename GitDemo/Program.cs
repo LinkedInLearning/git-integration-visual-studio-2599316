ebf4b6ff18f737cf3d859191507bd1bf98a968de
André Krämer <andre@andrekraemer.de>
2023-03-02 18:09:16 +0100

    Klasse TodoItem angelegt

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using GitDemo;
+
 Console.WriteLine("Hello, World!");
+
+var todoItems = new List<TodoItem>();
+
+var todo = new TodoItem { Id = 1, Title = "Anwendung fertigstellen", IsDone = false };
+todoItems.Add(todo);
+
+foreach (var item in todoItems)
+{
+    Console.WriteLine($"{item.Title}, Erledigt: {item.IsDone}");
+}
+Console.ReadLine();
---
+++ GitDemo/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
