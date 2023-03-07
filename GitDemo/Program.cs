11aac1390f6f870a21deb2461655835126464b19
André Krämer <andre@andrekraemer.de>
2023-03-07 20:53:49 +0100

    Code Cleanup in Program.cs

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -1,5 +1,4 @@
-﻿using GitDemo;
-using GitDemo.Repository;
+﻿using GitDemo.Repository;
 Console.WriteLine("Todo-Demo V1.0");
 Console.WriteLine("==============");
 Console.WriteLine();
@@ -8,10 +7,9 @@ var repository = new TodoRepository();
 
 var todos = repository.LoadTodos();
 
-foreach (var item in todos)
+foreach (var todo in todos)
 {
-    Console.WriteLine($"#{item.Id} - {item.Title}, ({(item.IsDone ? "erledigt [x]": "offen [ ]")})");
+    Console.WriteLine($"#{todo.Id} - {todo.Title}, ({(todo.IsDone ? "erledigt [x]": "offen [ ]")})");
 }
 Console.WriteLine("Bitte eine Taste drücken.");
 Console.ReadLine();
-// Einen Kommentar
